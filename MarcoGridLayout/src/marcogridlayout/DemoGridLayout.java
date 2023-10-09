349a4efff2b81338bc59e600b89c59951e317944
Yorne10 <125999396+Yorne10@users.noreply.github.com>
2023-10-09 09:12:19 -0600

    Add files via upload

---
+++ MarcoGridLayout/src/marcogridlayout/DemoGridLayout.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marcogridlayout;
+
+import javax.swing.JFrame;
+
+public class DemoGridLayout
+{
+
+    public static void main(String args[])
+    {
+        MarcoGridLayout marcoGridLayout = new MarcoGridLayout();
+        marcoGridLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoGridLayout.setSize(300, 200); // establece el tamaño del marco
+        marcoGridLayout.setVisible(true); // muestra el marco
+    } // fin de main
+} // fin de la clase DemoGridLayout
